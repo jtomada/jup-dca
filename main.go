b0c4786ac29f60f45d2d1c4f22a10e7088dad08c
allen0x1 <0xallenone@gmail.com>
2022-05-13 18:20:41 -0700

    add real query

--- main.go
+++ main.go
@@ -1,28 +1,72 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
-func main() {
-	fmt.Println("Hello World!")
+type Routes struct {
+	Routes []Route
+}
 
-	resp, err := http.Get("http://gobyexample.com")
+type Route struct {
+	InAmount              float64
+	OutAmount             float64
+	OutAmountWithSlippage float64
+	PriceImpactPct        float64
+	MarketInfos           []MarketInfo
+}
+
+type MarketInfo struct {
+	Id                 string
+	Label              string
+	InputMint          string
+	OutputMint         string
+	NotEnoughLiquidity bool
+	InAmount           float64
+	OutAmount          float64
+	PriceImpactPct     float64
+	LpFee              Fee
+	PlatformFee        Fee
+}
+
+type Fee struct {
+	Amount float64
+	Mint   string
+	Pct    float64
+}
+
+func main() {
+	fmt.Println("Hello Jupiter!")
+
+	base, err := url.Parse("https://quote-api.jup.ag")
+	if err != nil {
+		panic(err)
+	}
+
+	base.Path += "/v1/quote"
+
+	params := url.Values{}
+	params.Add("inputMint", "So11111111111111111111111111111111111111112")
+	params.Add("outputMint", "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
+	params.Add("amount", "1")
+	base.RawQuery = params.Encode()
+
+	fmt.Printf("Encoded URL is %q\n", base.String())
+
+	resp, err := http.Get(base.String())
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("Response status: ", resp.Status)
-
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
+	var j interface{}
+	err = json.NewDecoder(resp.Body).Decode(&j)
+	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("%s", j)
 }
