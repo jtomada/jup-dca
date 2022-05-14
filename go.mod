c250dd63a571676995167afdaac90b1b9b4fc3fd
allen0x1 <0xallenone@gmail.com>
2022-05-13 17:44:53 -0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/jup-api-go
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	resp, err := http.Get("http://gobyexample.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status: ", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
