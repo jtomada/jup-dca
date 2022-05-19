bfd9bac29f2e7b0c4db20f2f0c8a1bcff3d44668
allen0x1 <0xallenone@gmail.com>
2022-05-18 21:11:29 -0700

    clean up

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ use solana_sdk::{
     pubkey::Pubkey,
     signature::{read_keypair_file, Keypair, Signer},
 };
-use spl_token::{amount_to_ui_amount, ui_amount_to_amount};
+use spl_token::{
+    amount_to_ui_amount, 
+    ui_amount_to_amount,
+    native_mint
+};
 use std::{fs::File, time::Duration};
 use std::collections::HashMap;
 
@@ -17,6 +21,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _mint_addresses = HashMap::from([
         ("SOL", "So11111111111111111111111111111111111111112"),
         ("USDC", "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+        ("mSOL", "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So"),
     ]);
     let delay_timer = DelayTimerBuilder::default().build();
     let keypair = read_keypair_file(
@@ -26,8 +31,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = "./config.json";
     let file = File::open(path)?;
     let dca: DcaJobs = serde_json::from_reader(file)?;
+    let jobs = dca.jobs;
+    let mut i = 0;
 
-    for job in dca.jobs {
+    for job in jobs {
         println!("in: {} out: {} amt: {}", job.input_mint, job.output_mint, job.amount);
         let mut task_builder = TaskBuilder::default();
         
@@ -36,29 +43,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let kp_buf =  keypair_buf.clone();
             let kp = Keypair::from_bytes(&kp_buf).unwrap();
 
-            let input_mint = Pubkey::try_from("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v").unwrap();
-            let output_mint = Pubkey::try_from("So11111111111111111111111111111111111111112").unwrap();
-            let ui_amount = job.amount.clone();
+            let usdc_mint = Pubkey::try_from("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v").unwrap();
+            let _msol_mint = Pubkey::try_from("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So").unwrap();
+            let sol_mint = Pubkey::try_from("So11111111111111111111111111111111111111112").unwrap();
+           let ui_amount = job.amount.clone();
 
             async move {
                 let _ = swap(
-                    input_mint,
-                    output_mint,
+                    usdc_mint,
+                    sol_mint,
                     ui_amount,
                     1.0,
+                    false,
                     kp,
                 )
                 .await;
             }
         };
 
+        println!("Building task {}", i + 1);
+
         let task = task_builder
-            .set_task_id(1)
+            .set_task_id(i.try_into().unwrap())
             .set_frequency_repeated_by_seconds(60)
-            .set_maximum_parallel_runnable_num(2)
             .spawn_async_routine(body)?;
 
         delay_timer.add_task(task)?;
+        i = i + 1;
     }
 
     loop {
@@ -84,32 +95,39 @@ async fn swap(
     output_mint: Pubkey,
     ui_amount: f64,
     slippage: f64,
+    only_direct_routes: bool,
     keypair: Keypair,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let _msol = Pubkey::try_from("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So")?;
-
     let rpc_client = RpcClient::new_with_commitment(
         "https://solana-api.projectserum.com".into(),
         CommitmentConfig::confirmed(),
     );
 
-    println!(
-        "Pre-swap SOL balance: {}",
-        amount_to_ui_amount(
-            rpc_client.get_balance(&keypair.pubkey()).await?, 
-            9 
+    let sol_balance = amount_to_ui_amount(
+        rpc_client.get_balance(
+            &keypair.pubkey()
         )
+        .await
+        .expect("error getting sol balance"), 
+        native_mint::DECIMALS 
     );
+    println!("Pre-swap SOL balance: {}", sol_balance);
 
     let out_token_address = 
         spl_associated_token_account::get_associated_token_address(
-        
             &keypair.pubkey(), 
             &output_mint
         );
-    let out_ui_token = rpc_client.get_token_account(&out_token_address).await.expect("err get_token_acc").unwrap();    
-    let out_decimals = out_ui_token.token_amount.decimals;
-    let out_bal = out_ui_token.token_amount.ui_amount.unwrap();
+    let out_token_acc = rpc_client.get_token_account(
+        &out_token_address
+    )
+    .await
+    .expect(
+        "err get_token_acc"
+    )
+    .unwrap();    
+    let out_decimals = out_token_acc.token_amount.decimals;
+    let out_bal = out_token_acc.token_amount.ui_amount.unwrap();
     println!("Pre-swap output token balance: {}", out_bal);
 
     let in_token_address =
@@ -117,12 +135,18 @@ async fn swap(
             &keypair.pubkey(), 
             &input_mint
         );
-    let in_ui_token = rpc_client.get_token_account(&in_token_address).await.expect("err get_token_acc").unwrap(); 
-    let in_decimals = in_ui_token.token_amount.decimals;
-    let in_bal= in_ui_token.token_amount.ui_amount.unwrap();
+    let in_token_acc = rpc_client.get_token_account(
+        &in_token_address
+    )
+    .await
+    .expect(
+        "err get_token_acc"
+    )
+    .unwrap(); 
+    let in_decimals = in_token_acc.token_amount.decimals;
+    let in_bal= in_token_acc.token_amount.ui_amount.unwrap();
     println!("Pre-swap USDC balance: {}", in_bal);
     
-    let only_direct_routes = false;
     let quotes = jup_ag::quote(
         input_mint,
         output_mint,
@@ -159,7 +183,12 @@ async fn swap(
         setup,
         swap,
         cleanup,
-    } = jup_ag::swap(quote.clone(), keypair.pubkey()).await.expect("error getting swap");
+    } = jup_ag::swap(
+        quote.clone(), 
+        keypair.pubkey()
+    )
+    .await
+    .expect("error getting swap");
 
     let transactions = [setup, Some(swap), cleanup]
         .into_iter()
@@ -167,9 +196,17 @@ async fn swap(
         .collect::<Vec<_>>();
     println!("\nTransactions to send: {}", transactions.len());
 
-    for (i, mut transaction) in transactions.into_iter().enumerate() {
-        transaction.message.recent_blockhash = rpc_client.get_latest_blockhash().await.expect("error get_latest_blockhash");
-        transaction.sign(&[&keypair], transaction.message.recent_blockhash);
+    for (i, mut transaction) in transactions
+        .into_iter()
+        .enumerate() {
+        transaction.message.recent_blockhash = rpc_client
+            .get_latest_blockhash()
+            .await
+            .expect("error get_latest_blockhash");
+        transaction.sign(
+            &[&keypair], 
+            transaction.message.recent_blockhash
+        );
         println!(
             "Sending transaction {}: {}",
             i + 1,
@@ -177,7 +214,8 @@ async fn swap(
         );
         let signature = rpc_client
             .send_and_confirm_transaction_with_spinner(&transaction)
-            .await.expect("error send_and_confirm_tx");
+            .await
+            .expect("error send_and_confirm_tx");
         println!(
             "TX signature: {}: {}",
             i + 1,
@@ -187,16 +225,24 @@ async fn swap(
 
     println!(
         "Post-swap SOL balance: {}",
-        amount_to_ui_amount(rpc_client.get_balance(&keypair.pubkey()).await?, out_decimals)
+        amount_to_ui_amount(
+            rpc_client
+            .get_balance(&keypair.pubkey())
+            .await
+            .expect("err post swap sol balance"), 
+            out_decimals
+        )
     );
     println!(
         "Post-swap USDC balance: {}",
         amount_to_ui_amount(
             rpc_client
-                .get_token_account_balance(&in_token_address)
-                .await?
-                .amount
-                .parse::<u64>()?,
+            .get_token_account_balance(&in_token_address)
+            .await
+            .expect("err post out balance")
+            .amount
+            .parse::<u64>()
+            .expect("err parsing post out balance"),
             in_decimals 
         )
     );
